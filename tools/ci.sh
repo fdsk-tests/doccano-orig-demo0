1def0d8a01c6b9aa2b9c605a7ce42707d1ff03b5
Ali Gholami <ali@feedstock.com>
2019-06-21 17:11:28 +0200

    fix typo

--- tools/ci.sh
+++ tools/ci.sh
@@ -2,7 +2,7 @@
 
 set -o errexit
 
-python3 -m pip install -r /requirements.txt
+python3 -m pip install -r requirements.txt
 flake8
 python3 -m python app/manage.py migrate
 coverage run --source=app app/manage.py test server.tests
