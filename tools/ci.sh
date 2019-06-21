709fef5db0b6f04fd8fb7151121cfb8acaf2941d
Ali Gholami <ali@feedstock.com>
2019-06-21 17:15:33 +0200

    fix run python

--- tools/ci.sh
+++ tools/ci.sh
@@ -4,6 +4,6 @@ set -o errexit
 
 python3 -m pip install -r requirements.txt
 flake8
-python3 -m python app/manage.py migrate
+python3 app/manage.py migrate
 coverage run --source=app app/manage.py test server.tests
 coverage report
